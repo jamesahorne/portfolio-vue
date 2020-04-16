d7041f9ce431a23de1dc6523cf95ea378123d020
James Horne <james99h39@gmail.com>
2020-04-16 17:39:03 +0100

    ✅ Add home

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
     <Header/>
+    <main class="l-main">
+      <Home/>
+    </main>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Home from './components/Home'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Home
   }
 }
 </script>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home" id="home">
+    <div class="l-spacing">
+      <h1 class="home__title">
+        <span class="home__text home__text--primary">Hi, I'm James<span class="home__text--full-stop">.</span></span>
+        <span class="home__block home__block--primary"></span>
+        <span class="home__text home__text--secondary">I build websites<span class="home__text--full-stop">.</span></span>
+        <span class="home__block home__block--secondary"></span>
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
