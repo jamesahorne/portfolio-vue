127c1e3e0d51b06285cbdcb4775941c90247d07e
James Horne <james99h39@gmail.com>
2020-04-20 11:03:24 +0100

    ♻️ Refactor accordion js

--- client/src/components/Accordion.vue
+++ client/src/components/Accordion.vue
@@ -34,48 +34,43 @@ export default {
     'technologies'
   ],
   mounted() {
-    const accordionComponent = document.getElementById('accordion');
+    const allAccordions = document.getElementsByClassName('accordion__button');
+    const activeAccordions = document.getElementsByClassName('accordion__button accordion__button--is-active');
+    const firstAccordion = allAccordions[0];
+    const accordionHeight = firstAccordion.scrollHeight;
 
-    // If it doesn't exist you get a console error (if it isn't "undefined" and it isn't "null", then it exists)
-    if (typeof(accordionComponent) != 'undefined' && accordionComponent != null) {
-      const allAccordions = document.getElementsByClassName('accordion__button');
-      const activeAccordions = document.getElementsByClassName('accordion__button accordion__button--is-active');
-      const firstAccordion = allAccordions[0];
-      const accordionHeight = firstAccordion.scrollHeight;
+    for (let i = 0; i < allAccordions.length; i++) {
+      allAccordions[i].addEventListener('click', function() {
+        let isActiveAccordion = this.classList.contains('accordion__button--is-active');
+        let accordionSection = this.parentNode.parentNode;
+        let panel = this.parentNode.nextElementSibling;
+        let prevAccordion = activeAccordions[0] || firstAccordion;
+        let prevAccordionSection = prevAccordion.parentNode.parentNode;
+        let prevPanel = prevAccordion.parentNode.nextElementSibling;
+        const accordionBorders = document.getElementsByClassName('accordion__border');
 
-      for (let i = 0; i < allAccordions.length; i++) {
-        allAccordions[i].addEventListener('click', function() {
-          let isActiveAccordion = this.classList.contains('accordion__button--is-active');
-          let accordionSection = this.parentNode.parentNode;
-          let panel = this.parentNode.nextElementSibling;
-          let prevAccordion = activeAccordions[0] || firstAccordion;
-          let prevAccordionSection = prevAccordion.parentNode.parentNode;
-          let prevPanel = prevAccordion.parentNode.nextElementSibling;
-          const accordionBorders = document.getElementsByClassName('accordion__border');
+        // If active, close it, else, first close previous accordion and then open current one
+        if (isActiveAccordion) {
+          prevAccordion.classList.remove('accordion__button--is-active');
+          prevAccordion.setAttribute('aria-expanded', 'false');
+          prevPanel.style.maxHeight = null;
+          prevAccordionSection.style.height = accordionHeight + 'px';
+        } else {
+          prevAccordion.classList.remove('accordion__button--is-active');
+          prevPanel.style.maxHeight = null;
+          prevAccordionSection.style.height = accordionHeight + 'px';
+          prevAccordion.setAttribute('aria-expanded', 'false');
 
-          // If active, close it, else, first close previous accordion and then open current one
-          if (isActiveAccordion) {
-            prevAccordion.classList.remove('accordion__button--is-active');
-            prevAccordion.setAttribute('aria-expanded', 'false');
-            prevPanel.style.maxHeight = null;
-            prevAccordionSection.style.height = accordionHeight + 'px';
-          } else {
-            prevAccordion.classList.remove('accordion__button--is-active');
-            prevPanel.style.maxHeight = null;
-            prevAccordionSection.style.height = accordionHeight + 'px';
-            prevAccordion.setAttribute('aria-expanded', 'false');
+          this.classList.toggle('accordion__button--is-active');
+          this.setAttribute('aria-expanded', 'true');
+          panel.style.maxHeight = panel.scrollHeight + 24 + 'px';
+          accordionSection.style.height = panel.scrollHeight + accordionHeight + 'px';
+        }
 
-            this.classList.toggle('accordion__button--is-active');
-            this.setAttribute('aria-expanded', 'true');
-            panel.style.maxHeight = panel.scrollHeight + 24 + 'px';
-            accordionSection.style.height = panel.scrollHeight + accordionHeight + 'px';
-          }
-
-          for (var border of accordionBorders) {
-            border.style.height = accordionHeight - 32 + 'px';
-          }
-        });
-      }
+        for (var border of accordionBorders) {
+          border.style.height = accordionHeight - 32 + 'px';
+        }
+      });
     }
   }
 }
