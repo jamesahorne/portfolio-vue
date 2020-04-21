9b1c7cc3eab035a26ca0a39793a8a41fed723a0b
James Horne <james99h39@gmail.com>
2020-04-21 17:24:42 +0100

    ✏️♻️ Update about

--- client/src/components/About.vue
+++ client/src/components/About.vue
@@ -1,9 +1,11 @@
 <template>
-  <article class="about">
+  <article class="about" id="about">
     <div class="l-spacing">
       <h2 class="u-heading-1">Who am I?</h2>
       <p class="u-text">Hi, I'm James. I build websites and I like being creative with code.</p>
-      <p class="u-text">In my free time I'm a keen musician, having played acoustic guitar for over a decade and performing at different events. I also sing, play piano and write songs too.</p>
+      <p class="u-text">After finishing my Diploma in Software Development in 2019, I spent 6 months working as a web developer for a small agency. I focused on building front-end solutions (usually in the form of a pattern library).</p>
+      <p class="u-text">I'm now looking for my second role as a front-end developer. I'm familiar with Vue but am interested in learning other frameworks. I have some backend knowledge/experience too.</p>
+      <p class="u-text u-text--highlighted">In my free time I'm a keen musician, having played acoustic guitar for over a decade and performing at various events. I also sing, play piano and write songs too.</p>
     </div>
   </article>
 </template>
