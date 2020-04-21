768dccb8c3aceeda677a1177cf7d9c83d301c956
James Horne <james99h39@gmail.com>
2020-04-21 17:29:52 +0100

    ♻️ Add brand href

--- client/src/components/Brand.vue
+++ client/src/components/Brand.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="brand">
-    <a class="brand__link" href="#">james<span class="brand__link--highlighted">a</span>horne<span class="brand__link--highlighted brand__link--colour">.</span>dev</a>
+    <a class="brand__link" href="https://www.jamesahorne.dev">james<span class="brand__link--highlighted">a</span>horne<span class="brand__link--highlighted brand__link--colour">.</span>dev</a>
   </div>
 </template>
 
