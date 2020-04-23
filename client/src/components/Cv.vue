9879fac8deb3e50d451a5fa1af1a5d5ff136a7e7
James Horne <james99h39@gmail.com>
2020-04-23 11:59:26 +0100

    ♻️ Add cv LinkedIn link

--- client/src/components/Cv.vue
+++ client/src/components/Cv.vue
@@ -58,6 +58,7 @@
               <div class="cv__content">
                 <ul class="cv__list cv__list--icons cv__list--big">
                   <li class="cv__portfolio"><a class="u-link u-link--cv" href="https://www.jamesahorne.dev">www.jamesahorne.dev</a></li>
+                  <li class="cv__linked-in"><a class="u-link u-link--cv" href="https://www.linkedin.com/in/jamesahorne/">jamesahorne</a></li>
                   <li class="cv__github"><a class="u-link u-link--cv" href="https://github.com/jamesahorne/">jamesahorne</a></li>
                   <li class="cv__email"><a class="u-link u-link--cv" href="mailto:james99h39@gmail.com">james99h39@gmail.com</a></li>
                   <li class="cv__phone">07531 180950</li>
