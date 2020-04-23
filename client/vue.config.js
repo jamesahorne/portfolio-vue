45c9205815e7bff3b5dcf76774f5e1b5dfdb4603
James Horne <james99h39@gmail.com>
2020-04-23 12:00:10 +0100

    ♻️ Use webpack to add meta data

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,22 @@
 const path = require('path')
 
 module.exports = {
-  outputDir: path.resolve(__dirname, '../server/public')
+  outputDir: path.resolve(__dirname, '../server/public'),
+  chainWebpack: (config) => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'jamesahorne.dev | Front-end developer';
+        args[0].meta = {
+          'author': 'James Horne',
+          'description': 'Hi, I\'m James. I build websites. I\'m a front-end web developer and this is my personal site. Interested? Contact me!',
+          'og:url': 'https://www.jamesahorne.dev/',
+          'og:type': 'website',
+          'og:title': 'jamesahorne.dev | Front-end developer',
+          'og:description': 'Hi, I\'m James. I build websites. I\'m a front-end web developer and this is my personal site. Interested? Contact me!',
+          'og:image': 'https://www.jamesahorne.dev/img/my-portfolio.b1e45ffc.png'
+        };
+        return args;
+      })
+  }
 }
