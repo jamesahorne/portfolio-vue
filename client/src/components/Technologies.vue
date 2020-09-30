43d269f4c7c5497b73cb0802def3817076835400
James Horne <james99h39@gmail.com>
2020-09-30 11:34:19 +0100

    ✏️ Update technologies and about

--- client/src/components/Technologies.vue
+++ client/src/components/Technologies.vue
@@ -4,7 +4,7 @@
       <h2 class="u-heading-1">What are my tools and tech?</h2>
       <p class="u-text">
         Here's a list of some the main tools and tech that I use.
-        <br>
+        <br />
         For more info see my CV.
       </p>
       <div class="technologies__accordion">
@@ -15,33 +15,36 @@
 </template>
 
 <script>
-import Accordion from './Accordion'
+import Accordion from "./Accordion";
 
 export default {
-  name: 'Techonologies',
+  name: "Techonologies",
   components: {
-    Accordion
+    Accordion,
   },
   data() {
     return {
       technologies: [
         {
-          "id": "vue",
-          "heading": "Vue.js",
-          "contents": "I'm teaching myself Vue at the moment to learn a front-end framework. This site is built with Vue, with more coming soon!"
+          id: "react",
+          heading: "React.js",
+          contents:
+            "I'm learning React while looking for work. For my first project, I'm rebuilding my portfolio site with React, so watch this space.",
         },
         {
-          "id": "express",
-          "heading": "Express",
-          "contents": "I'm teaching myself Express too. This site uses Express for some routing and middleware."
+          id: "vue",
+          heading: "Vue.js",
+          contents:
+            "This site is built with Vue! This will become a secondary version of my portfolio site as the main version, once built, will be built with React.",
         },
         {
-          "id": "pattern-lab",
-          "heading": "Pattern Lab",
-          "contents": "I build a pattern library for every major project I do, using Gulp and Browsersync for tasks such as watching files and compiling SCSS into minified, autoprefixed CSS."
-        }
-      ]
-    }
-  }
-}
+          id: "pattern-lab",
+          heading: "Pattern Lab",
+          contents:
+            "I build a pattern library for every major project I do, using Gulp and Browsersync for tasks such as watching files and compiling SCSS into minified, autoprefixed CSS.",
+        },
+      ],
+    };
+  },
+};
 </script>
